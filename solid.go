90d094d48722f2c8aead368407e001a4e165d67b
mububoki <67190861+mububoki@users.noreply.github.com>
2020-12-10 20:50:08 +0900

    fix comment

--- solid.go
+++ solid.go
@@ -5,7 +5,7 @@ import (
 	"image/color"
 )
 
-//NewUniform returns a new Solid image of the given bounds and color.
+// SolidImage returns a new Solid image of the given bounds and color.
 func SolidImage(rectangle image.Rectangle, c color.Color) image.Image {
 	return image.Image(image.NewPaletted(rectangle, []color.Color{c}))
 }
