c5f8e560324aea37818beef4cb1acc7cb1976579
mububoki <67190861+mububoki@users.noreply.github.com>
2020-12-10 20:27:14 +0900

    support png

---
+++ png/writer.go
@@ -0,0 +1,20 @@
+package png
+
+import (
+	"image"
+	"image/color"
+	"image/png"
+	"io"
+
+	"github.com/mububoki/graffiti"
+)
+
+// EncodeSolid writes the random image with the rectangle and the color c to w in PNG format.
+func EncodeSolid(w io.Writer, rectangle image.Rectangle, c color.Color) error {
+	return png.Encode(w, graffiti.SolidImage(rectangle, c))
+}
+
+// EncodeRandom writes the random image with the rectangle to w in PNG format.
+func EncodeRandom(w io.Writer, rectangle image.Rectangle) error {
+	return png.Encode(w, graffiti.RandomImage(rectangle))
+}
